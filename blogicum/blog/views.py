306e5cf839c803b89c1742232ce2a211bd74bf64
kazantsevnik <n.a.kazantsev@urfu.me>
2024-12-15 22:22:25 +0500

    stage 2

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 posts = [
     {
@@ -26,12 +25,15 @@ posts = [
     },
 ]
 
+
 def index(request):
     return render(request, 'blog/index.html', {'posts': posts})
+
 
 def post_detail(request, id):
     post = next((post for post in posts if post['id'] == id), None)
     return render(request, 'blog/detail.html', {'post': post})
 
+
 def category_posts(request, category_slug):
-    return render(request, 'blog/category.html', {'category_slug': category_slug})
+    return render(request, 'category.html', {'category': category_slug})
