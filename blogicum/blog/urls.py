6578f8a427aa40e1917eba1873fb0990c9157f6d
kazantsevnik <n.a.kazantsev@urfu.me>
2024-12-16 00:10:40 +0500

    doyou

--- blogicum/blog/urls.py
+++ blogicum/blog/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'blog'
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('posts/<int:id>/', views.post_detail, name='detail'),
-    path('category/<slug:category_slug>/', views.category_posts, name='category'),
+path('', views.index, name='index'),
+path('posts/<int:id>/', views.post_detail, name='detail'),
+path('category/<slug:category_slug>/', views.category_posts, name='category'),
 ]
