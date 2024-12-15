ca901e75c049b9153d00fb2c99ca61b8353bcc9a
kazantsevnik <n.a.kazantsev@urfu.me>
2024-12-16 00:21:02 +0500

    rr

--- blogicum/blog/urls.py
+++ blogicum/blog/urls.py
@@ -5,10 +5,6 @@ app_name = 'blog'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('posts/<int:id>/',
-      views.post_detail,
-        name='detail'),
-    path('category/<slug:category_slug>/',
-          views.category_posts, 
-          name='category'),
+    path('posts/<int:id>/', views.post_detail, name='detail'),
+    path('category/<slug:category_slug>/', views.category_posts, name='category'),
 ]
